c3babe543fb104eab7e3d51455307a9a61062772
ajk0421 <ajk.spirituallounge@gmai.com>
2021-10-21 23:24:58 +0900

    問1修正

--- q1/method.rb
+++ q1/method.rb
@@ -2,31 +2,27 @@ require './base.rb'
 
 module Q1
   class Method < Base
-    class << self
+    def self.execute
+      puts "お名前を入力してください"
+      name = set_name
 
-      def disp_name(naem)
-        "#{@name}さん、こんにちわ！！"
-      end
+      return puts "文字列で入力してください" if Method.get_string?(name)
 
-      def set_name
-        name = gets.chomp
+      puts disp_name(name)
+    end
 
-        name
-      end
+    def self.disp_name(naem)
+      "#{@name}さん、こんにちわ！！"
+    end
 
-      def get_string?(target)
-        target.is_a?(String)
-      end
+    def self.set_name
+      self.name = gets.chomp
 
-      def execute
-        puts "お名前を入力してください"
-        name = set_name
+      name
+    end
 
-        return puts "文字列で入力してください" if Method.get_string?(name)
-
-        puts disp_name(name)
-      end
-
+    def self.get_string?(target)
+      target.is_a?(String)
     end
   end
 end
