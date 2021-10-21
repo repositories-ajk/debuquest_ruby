2c7956087cf55021c868793561b94b1090d6d61d
ajk0421 <ajk.spirituallounge@gmai.com>
2021-10-21 23:30:20 +0900

    問1再修正

--- q1/method.rb
+++ q1/method.rb
@@ -6,23 +6,26 @@ module Q1
       puts "お名前を入力してください"
       name = set_name
 
-      return puts "文字列で入力してください" if Method.get_string?(name)
+      return puts "文字列で入力してください" if get_string?(name)
 
-      puts disp_name(name)
+      disp_name(name)
     end
 
     def self.disp_name(naem)
-      "#{@name}さん、こんにちわ！！"
+      puts "#{@name}さん、こんにちわ！！"
     end
+    private_class_method :disp_name
 
     def self.set_name
       self.name = gets.chomp
 
-      name
+      @name
     end
+    private_class_method :set_name
 
     def self.get_string?(target)
       target.is_a?(String)
     end
+    private_class_method :get_string?
   end
 end
