9c90b15f3e68245419fda32e8f8b8ab909348193
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2021-11-13 16:19:10 +0900

    Q0の模範回答

--- q0/method.rb
+++ q0/method.rb
@@ -1,12 +1,14 @@
-require './base.rb'
+require "./base.rb"
 
 class Q0::Method < Base
   def self.execute
     puts "好きな数字を入力してください"
-    num_1 = gets.chomp
+    # 文字列なので数字に変換する
+    num_1 = gets.chomp.to_i
 
     puts "もう一度数字を入力してください"
-    num_2 = gets.chomp
+    # 文字列なので数字に変換する
+    num_2 = gets.chomp.to_i
 
     puts "足し算します"
 
