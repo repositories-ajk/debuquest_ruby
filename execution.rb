19fe4d57c8d12f24945598df96c1ad3371dac134
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2021-11-13 17:00:24 +0900

    問題の呼び出し方法のリファクタリング

--- execution.rb
+++ execution.rb
@@ -1,11 +1,11 @@
 require "pry"
-require "./q0/method"
-require "./q1/method"
-require "./q2/method"
+require "./q0/main"
+require "./q1/main"
+require "./q2/main"
 require "./q3/main"
 require "./q4/main"
-require "./q5/method"
-require "./q6/method"
+require "./q5/main"
+require "./q6/main"
 require "./q7/main"
 require "./q8/main"
 require "./q9/main"
@@ -13,71 +13,25 @@ require "./q9/main"
 class Execution
   QUESTION_NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-  class << self
+  # ここから下は絶対触らないでね！(あじーより)
+  def self.call
+    puts "挑戦する問題番号を入力してください！！"
 
-    # 各問題ごとに実行されるメソッド
-    def q0
-      Q0::Method.execute
-    end
+    question_number = gets.chomp.to_i
+    validate_msg = validate(question_number)
+    return puts validate_msg if !!validate_msg
 
-    def q1
-      Q1::Method.execute
-    end
-
-    def q2
-      Q2::Method.execute
-    end
-
-    def q3
-      Q3::Main.execute
-    end
-
-    def q4
-      Q4::Main.execute
-    end
-
-    def q5
-      Q5::Method.execute
-    end
-
-    def q6
-      Q6::Method.execute
-    end
-
-    def q7
-      Q7::Main.execute
-    end
-
-    def q8
-      Q8::Main.execute
-    end
-
-    def q9
-      Q9::Main.execute
-    end
-
-    # ここから下は絶対触らないでね！(あじーより)
-    def call
-      puts "挑戦する問題番号を入力してください！！"
-
-      q_num = gets.chomp.to_i
-
-      validate_msg = validate(q_num)
-
-      return puts validate_msg if !!validate_msg
-
-      Execution.public_send("q#{q_num}")
-    end
-
-    private
-
-    def validate(num)
-      return "存在しない問題番号です" if !QUESTION_NUMS.include?(num)
-      return "半角数字で入力してください" if !num.is_a?(Integer)
-
-      nil
-    end
+    klass = Object.const_get("Q#{question_number}::Main")
+    klass.execute
   end
+
+  def self.validate(num)
+    return "存在しない問題番号です" if !QUESTION_NUMS.include?(num)
+    return "半角数字で入力してください" if !num.is_a?(Integer)
+
+    nil
+  end
+  private_class_method :validate
 end
 
 
