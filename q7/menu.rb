3505f013317f2a5652ad578344f29a9de730202d
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2021-11-14 15:50:31 +0900

    Q7の問題を修正

--- q7/menu.rb
+++ q7/menu.rb
@@ -9,21 +9,22 @@ class Q7::Menu
   end
 
   def info
-    text = "#{name} #{price}円"
+    "#{name} #{price}円"
   end
 
   def total_price(count)
     total_price = price * count
-    total_price = discount(total_price: total_price, count: count)
+    total_price = discount_price(count)
     total_price
   end
 
   private
 
-    def discount(total_price:, count:)
-      total_price -= 100 if count >= 3
-      total_price -= 100 if is_discount_day?
-      total_price
+    def discount_price(count)
+      discount_price = 0
+      discount_price -= 100 if count >= 3
+      discount_price -= 100 if is_discount_day?
+      discount_price
     end
 
     def is_discount_day?
