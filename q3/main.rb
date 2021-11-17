24be81dc5dc1c00532815c0f751a09fee5cc97c0
k_akihiro <k.shuhari@gmail.com>
2021-11-17 20:24:58 +0900

    #3 出力の変更

--- q3/main.rb
+++ q3/main.rb
@@ -4,12 +4,13 @@ require "./q3/sample_api.rb"
 class Q3::Main < Base
   def self.execute
     client = Q3::SampleApi.new
-    base_info = client.get_sample_user_info(type: :default)
+    base_info = client.get_sample_user_info(type: :base)
     detail_info = client.get_sample_user_info(type: :detail)
 
-    user_info = base_info
-    user_id_result = "対象のユーザーの id は #{user_info["id"]} です。"
-    user_country_result = "対象のユーザーの country は #{user_info["country"]} です。"
+    user_info = base_info.merge(detail_info)
+
+    user_id_result = "対象のユーザーの id は #{user_info[:id]} です。"
+    user_country_result = "対象のユーザーの country は #{user_info[:country]} です。"
 
     puts user_id_result
     puts user_country_result
