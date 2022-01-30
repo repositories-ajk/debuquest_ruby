e0b041cba475978d53f1733e886570303cb18267
島田大暉 <hiro.alo.f1@gmail.com>
2022-01-30 17:49:13 +0900

    to_iの追加

--- q0/main.rb
+++ q0/main.rb
@@ -3,10 +3,10 @@ require "./base.rb"
 class Q0::Main < Base
   def self.execute
     puts "好きな数字を入力してください"
-    num_1 = gets.chomp
+    num_1 = gets.chomp.to_i
 
     puts "もう一度数字を入力してください"
-    num_2 = gets.chomp
+    num_2 = gets.chomp.to_i
 
     puts "足し算します"
 
